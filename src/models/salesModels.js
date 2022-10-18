30b260ef27479ba347cdfb2c9bd96b7f8bdfd15f
magnogouvea <magnogouvea@gmail.com>
2022-10-18 18:57:13 -0300

    Fix: req06

--- src/models/salesModels.js
+++ src/models/salesModels.js
@@ -3,12 +3,10 @@ const db = require('./db');
 const newSale = async (sale) => {
   console.log({ sale });
   const [date] = await db.execute('INSERT INTO sales (date) VALUE (now())');
-  sale.forEach(async (sales) => {
-    await db.execute(
-      'INSERT INTO sales_products (sale_id, product_id, quantity) VALUE (?, ?, ?)',
-      [date.insertId, sales.productId, sales.quantity],
-    );
-  });
+  await Promise.all(sale.map((sales) => db.execute(
+    'INSERT INTO sales_products (sale_id, product_id, quantity) VALUE (?, ?, ?)',
+    [date.insertId, sales.productId, sales.quantity],
+  ))); // com ajuda do André 
   return { id: date.insertId, itemsSold: sale };
 };
 
